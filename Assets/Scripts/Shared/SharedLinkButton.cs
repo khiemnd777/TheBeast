41a2a1fc822df17271e005e3cfe9462df1529142
khiem.nguyen <knguyen20@wellbarrier.slb.com>
2021-11-07 17:52:09 +0700

    shared link buttons

---
+++ Assets/Scripts/Shared/SharedLinkButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class SharedLinkButton : MonoBehaviour
+{
+  public string baseUrl;
+  public string url;
+
+  public void Click()
+  {
+    if (!string.IsNullOrEmpty(url))
+    {
+      Application.OpenURL($"{baseUrl}{UnityWebRequest.EscapeURL(url)}");
+    }
+  }
+}
